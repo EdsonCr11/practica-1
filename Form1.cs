10085da8086deb487bc31049aab2fe00deffff57
DELL-1 <DELL-1@DESKTOP-GAMS896>
2023-11-28 17:28:17 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_1
+{
+    public partial class Gif_Animado : Form
+    {
+        public Gif_Animado()
+        {
+            InitializeComponent();
+        }
+    }
+}
